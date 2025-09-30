8813e7b921839c69882d3d5b67c7a51bc1cfa3e0
Vicky <147834352+Vics055@users.noreply.github.com>
2025-10-01 03:20:08 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,103 @@
+function showInfo(title, content) {
+  document.getElementById("info-title").innerText = title;
+  document.getElementById("info-content").innerText = content;
+  document.getElementById("info-box").style.display = "block";
+}
+
+
+ function showInfo() {
+    const aboutSection = document.getElementById("info-box");
+    aboutSection.style.display = "block";
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+  }
+
+  
+function showInfo1(titleskill, contentskill) {
+  document.getElementById("info-title-skill").innerText = titleskill;
+  document.getElementById("info-content-skill").innerText = contentskill;
+  document.getElementById("info-box-skill").style.display = "block";
+}
+
+
+function showInfo1() {
+    const aboutSection = document.getElementById("info-box-skill");
+    aboutSection.style.display = "block";
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+  }
+
+  
+
+
+function showInfo2(titleproject, contentproject) {
+  document.getElementById("info-title-project").innerText = titleproject;
+  document.getElementById("info-content-project").innerText = contentproject;
+  document.getElementById("info-box-project").style.display = "block";
+}
+
+
+function showInfo2() {
+    const aboutSection = document.getElementById("info-box-project");
+    aboutSection.style.display = "block";
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+  }
+
+
+
+function showInfo3(titlecertification, contentcertification) {
+  document.getElementById("info-title-certification").innerText = titlecertification;
+  document.getElementById("info-content-certification").innerText = contentcertification;
+  document.getElementById("info-box-certification").style.display = "block";
+}
+
+
+function showInfo3() {
+    const aboutSection = document.getElementById("info-box-certification");
+    aboutSection.style.display = "block";
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+  }
+
+
+
+function showInfo4(titleaward, contentaward) {
+  document.getElementById("info-title-award").innerText = titleaward;
+  document.getElementById("info-content-award").innerText = contentaward;
+  document.getElementById("info-box-award").style.display = "block";
+}
+
+
+function showInfo4() {
+    const aboutSection = document.getElementById("info-box-award");
+    aboutSection.style.display = "block";
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+  }
+
+
+
+
+function showInfo5(titlehobby, contenthobby) {
+  document.getElementById("info-title-hobby").innerText = titlehobby;
+  document.getElementById("info-content-hobby").innerText = contenthobby;
+  document.getElementById("info-box-hobby").style.display = "block";
+}
+
+
+function showInfo5() {
+    const aboutSection = document.getElementById("info-box-hobby");
+    aboutSection.style.display = "block";
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+  }
+
+
+
+function showInfo6(titlecontact, contentcontact) {
+  document.getElementById("info-title-contact").innerText = titlecontact;
+  document.getElementById("info-content-contact").innerText = contencontact;
+  document.getElementById("info-box-contact").style.display = "block";
+}
+
+
+function showInfo6() {
+    const aboutSection = document.getElementById("info-box-contact");
+    aboutSection.style.display = "block";
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+  }
